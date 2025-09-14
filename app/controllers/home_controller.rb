ff0eaf3ab6b5c6c5a255a5775169649760b5ec5e
Hussein6685 <husseinail6685@gmail.com>
2025-09-14 22:53:07 +0300

    add username field to users table & start building home feed algorithm

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-  @posts = Post.all()
+    @posts = []
+    if current_user
+      @posts = Post.where.not(user_id: current_user.id).order(created_at: :desc)
+    end
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
