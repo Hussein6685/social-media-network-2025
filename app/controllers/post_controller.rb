2463ad5e596eed5bc2cea053e50f5cc18ea40ed4
Hussein6685 <husseinail6685@gmail.com>
2025-09-14 20:17:50 +0300

    add upload image feather

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,11 @@
 class PostController < ApplicationController
   def create
-    Post.create(user_id: current_user.id, post_text: params[:post_text], has_image: false)
-    redirect_to request.referrer, notice: "تم إنشاء المنشور بنجاح.", allow_other_host: true
-
-
+    post = Post.create(user_id: current_user.id, post_text: params[:post_text], has_image: false)
+    if post.save
+      post.image.attach(params[:image]) if params[:image].present?
+      post.update(has_image: true) if params[:image].present?
+      redirect_to request.referrer, notice: "تم إنشاء المنشور بنجاح.", allow_other_host: true
+    end
   end
 end
 #   def create
