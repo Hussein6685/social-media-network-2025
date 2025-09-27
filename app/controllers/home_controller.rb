daf5191e0b1f0426c9cf250dbb4eeffa4de6eeba
Hussein6685 <husseinail6685@gmail.com>
2025-09-27 17:05:27 +0300

    add comments to news feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @posts = []
+    @feeds = []
     if current_user
-      @posts = Post.where.not(user_id: current_user.id).order(created_at: :desc)
+      posts = Post.where.not(user_id: current_user.id).order(created_at: :desc)
+      comments = Comment.where.not(user_id: current_user.id).order(created_at: :desc)
+      all_add_activities = posts + comments
+      @feeds = all_add_activities.sort_by(&:created_at).reverse
     end
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
