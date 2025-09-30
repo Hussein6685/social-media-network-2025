38f397a8e48d5c4bb509f51a600fb0cbd2e86106
Hussein6685 <husseinail6685@gmail.com>
2025-09-30 22:33:13 +0300

    implement user authentication before accessing home index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  private
+  def authenticate_user!
+    redirect_to new_user_session_path unless current_user
+  end
 end
